e795e984025906fe2995bdb54726ef0e8c9572b6
John Paul Pageler Jr <harakido1@yahoo.com>
2021-12-19 01:27:21 -0500

    Added nodemon to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-
-const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const colors = require("colors");
@@ -9,21 +7,27 @@ const app = express();
 dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("personal-note-library API is running on port 5000...");
+  res.send("personal-note-library API is running on port 5000...");
 });
 
 // API endpoint for all ./data/notes
 app.get("/api/notes", (req, res) => {
-    res.json(notes);
+  res.json(notes);
 });
 
 // API endpoint for a specific ./data/notes/note
 app.get("/api/notes/:id", (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id);
+  const note = notes.find((n) => n._id === req.params.id);
 
-    res.send(note);
+  res.send(note);
 });
 
 const PORT = process.env.PORT || 8000; // Variable for .env port to use in template string below
 
-app.listen(PORT, console.log(`Server running on PORT ${PORT}...`.yellow.bold));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}...`.yellow
+      .bold
+  )
+);
